71c97c29b2bbe4c34a8f892f9dba29d04062d051
adam <athornton@lsst.org>
2021-09-10 20:03:58 -0700

    WIP

--- src/query.ts
+++ src/query.ts
@@ -2,7 +2,7 @@
 // Distributed under the terms of the MIT License.
 
 import {
-  Menu
+  Menu, Widget
 } from '@lumino/widgets';
 
 import {
@@ -29,10 +29,6 @@ import {
 import {
   PageConfig
 } from '@jupyterlab/coreutils';
-
-import {
-  Widget
-} from '@lumino/widgets';
 
 /**
  * The command IDs used by the plugin.
