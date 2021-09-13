73e70d8111a2eb1ea5a0ff1610ccabd6589811ed
adam <athornton@lsst.org>
2021-09-13 15:04:13 -0700

    WIP

--- rsp_jupyter_extensions/__init__.py
+++ rsp_jupyter_extensions/__init__.py
@@ -9,7 +9,7 @@ def _jupyter_server_extension_paths():
     """
     Function to declare Jupyter Server Extension Paths.
     """
-    exts = ("displayversion", "environment", "hub_comm", "query")
+    exts = ("displayversion", "environment", "hub", "query")
     # This comprehension actually works, but black can't handle it!
     # return [ {"module": f"rsp_jupyter_extensions.{ext}"} for ext in exts ]
     elist = []
@@ -22,5 +22,5 @@ def load_jupyter_server_extension(nbapp):
     """
     Function to load Jupyter Server Extension.
     """
-    nbapp.log.info("Loading rubin environment server extension.")
+    nbapp.log.info("Loading RSP server extensions.")
     setup_handlers(nbapp.web_app)
