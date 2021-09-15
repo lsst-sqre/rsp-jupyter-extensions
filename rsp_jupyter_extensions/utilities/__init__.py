8d48c259a320cda51eb1319a9fba2755e89e000c
adam <athornton@lsst.org>
2021-09-15 12:23:22 -0700

    Update __init__.py with logging addition; reorder

--- rsp_jupyter_extensions/utilities/__init__.py
+++ rsp_jupyter_extensions/utilities/__init__.py
@@ -4,26 +4,29 @@ rsp_jupyter_utils.helper
 """
 from .catalog import get_tap_service, get_catalog, retrieve_query
 from .forwarder import Forwarder
+from .logging import IPythonHandler, forward_lsst_log
 from .utils import (
-    format_bytes,
-    get_hostname,
-    show_with_bokeh_server,
-    get_pod,
-    get_node,
-    get_digest,
     get_access_token,
+    format_bytes,
+    get_digest,
+    get_hostname,
+    get_node,
+    get_pod,
+    show_with_bokeh_server,
 )
 
 __all__ = [
     Forwarder,
+    IPythonHandler,
     format_bytes,
+    forward_lsst_log,
+    get_access_token,
     get_catalog,
+    get_digest,
+    get_node,
+    get_pod,
     get_tap_service,
     retrieve_query,
     get_hostname,
     show_with_bokeh_server,
-    get_pod,
-    get_node,
-    get_digest,
-    get_access_token,
 ]
