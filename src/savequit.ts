1bb7b293ec7df570c656bba6efd4577be60d6227
adam <athornton@lsst.org>
2023-01-24 13:55:06 -0700

    Fix ts/tsx linting

--- src/savequit.ts
+++ src/savequit.ts
@@ -1,59 +1,47 @@
 // Copyright (c) LSST DM/SQuaRE
 // Distributed under the terms of the MIT License.
 
-import {
-  Menu
-} from '@lumino/widgets';
+import { Menu } from '@lumino/widgets';
+
+import { showDialog, Dialog } from '@jupyterlab/apputils';
+
+import { IMainMenu } from '@jupyterlab/mainmenu';
 
 import {
-  showDialog, Dialog
-} from '@jupyterlab/apputils';
-
-import {
-  IMainMenu
-} from '@jupyterlab/mainmenu';
-
-import {
-  JupyterFrontEnd, JupyterFrontEndPlugin
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
-import {
-  IDocumentManager
-} from '@jupyterlab/docmanager';
+import { IDocumentManager } from '@jupyterlab/docmanager';
 
-import {
-  PageConfig
-} from '@jupyterlab/coreutils';
+import { PageConfig } from '@jupyterlab/coreutils';
 
-import {
-  ServiceManager, ServerConnection
-} from '@jupyterlab/services';
+import { ServiceManager, ServerConnection } from '@jupyterlab/services';
 
-import {
-  each
-} from '@lumino/algorithm';
+import { each } from '@lumino/algorithm';
 
-import * as token from "./tokens";
+import * as token from './tokens';
 
 /**
  * The command IDs used by the plugin.
  */
-export
-namespace CommandIDs {
-  export const justQuit: string = 'justquit:justquit';
-  export const saveQuit: string = 'savequit:savequit';
-  export const saveLogout: string = 'savelogout:savelogout';
-};
-
+export namespace CommandIDs {
+  export const justQuit = 'justquit:justquit';
+  export const saveQuit = 'savequit:savequit';
+  export const saveLogout = 'savelogout:savelogout';
+}
 
 /**
  * Activate the jupyterhub extension.
  */
-export function activateRSPSavequitExtension(app: JupyterFrontEnd, mainMenu: IMainMenu, docManager: IDocumentManager): void {
-
+export function activateRSPSavequitExtension(
+  app: JupyterFrontEnd,
+  mainMenu: IMainMenu,
+  docManager: IDocumentManager
+): void {
   console.log('rsp-savequit: loading...');
 
-  let svcManager = app.serviceManager;
+  const svcManager = app.serviceManager;
 
   const { commands } = app;
 
@@ -61,7 +49,7 @@ export function activateRSPSavequitExtension(app: JupyterFrontEnd, mainMenu: IMa
     label: 'Exit Without Saving',
     caption: 'Destroy container',
     execute: () => {
-      justQuit(app, docManager, svcManager, false)
+      justQuit(app, docManager, svcManager, false);
     }
   });
 
@@ -69,7 +57,7 @@ export function activateRSPSavequitExtension(app: JupyterFrontEnd, mainMenu: IMa
     label: 'Save All and Exit',
     caption: 'Save open notebooks and destroy container',
     execute: () => {
-      saveAndQuit(app, docManager, svcManager, false)
+      saveAndQuit(app, docManager, svcManager, false);
     }
   });
 
@@ -77,95 +65,109 @@ export function activateRSPSavequitExtension(app: JupyterFrontEnd, mainMenu: IMa
     label: 'Save All, Exit, and Log Out',
     caption: 'Save open notebooks, destroy container, and log out',
     execute: () => {
-      saveAndQuit(app, docManager, svcManager, true)
+      saveAndQuit(app, docManager, svcManager, true);
     }
   });
 
   // Add commands and menu itmes.
-  let menu: Menu.IItemOptions[] =
-    [
-      { command: CommandIDs.justQuit },
-      { command: CommandIDs.saveQuit },
-      { command: CommandIDs.saveLogout },
-    ]
+  const menu: Menu.IItemOptions[] = [
+    { command: CommandIDs.justQuit },
+    { command: CommandIDs.saveQuit },
+    { command: CommandIDs.saveLogout }
+  ];
   // Put it at the bottom of file menu
-  let rank = 150;
+  const rank = 150;
   mainMenu.fileMenu.addGroup(menu, rank);
 
   console.log('rsp-savequit: ...loaded.');
 }
 
 function hubDeleteRequest(app: JupyterFrontEnd): Promise<Response> {
-  let svcManager = app.serviceManager;
-  let settings = svcManager.serverSettings
-  let endpoint = PageConfig.getBaseUrl() + "rubin/hub"
-  let init = {
-    method: "DELETE",
-  }
-  console.log("hubRequest: URL: ", endpoint, " | Settings:", settings)
-  return ServerConnection.makeRequest(endpoint, init, settings)
+  const svcManager = app.serviceManager;
+  const settings = svcManager.serverSettings;
+  const endpoint = PageConfig.getBaseUrl() + 'rubin/hub';
+  const init = {
+    method: 'DELETE'
+  };
+  console.log('hubRequest: URL: ', endpoint, ' | Settings:', settings);
+  return ServerConnection.makeRequest(endpoint, init, settings);
 }
 
-function saveAll(app: JupyterFrontEnd, docManager: IDocumentManager, svcManager: ServiceManager): Promise<any> {
-  let promises: Promise<any>[] = [];
+function saveAll(
+  app: JupyterFrontEnd,
+  docManager: IDocumentManager,
+  svcManager: ServiceManager
+): Promise<any> {
+  const promises: Promise<any>[] = [];
   each(app.shell.widgets('main'), widget => {
     if (widget) {
-      let context = docManager.contextForWidget(widget);
+      const context = docManager.contextForWidget(widget);
       if (context) {
-        console.log("Saving context for widget:", { id: widget.id })
-        promises.push(context.save())
+        console.log('Saving context for widget:', { id: widget.id });
+        promises.push(context.save());
       } else {
-        console.log("No context for widget:", { id: widget.id })
+        console.log('No context for widget:', { id: widget.id });
       }
     }
-  })
-  console.log("Waiting for all save-document promises to resolve.")
-  let r = Promise.resolve(1)
+  });
+  console.log('Waiting for all save-document promises to resolve.');
+  let r = Promise.resolve(1);
   if (promises) {
     Promise.all(promises);
-    r = promises[0]
+    r = promises[0];
   }
-  return r
+  return r;
 }
 
-
-function saveAndQuit(app: JupyterFrontEnd, docManager: IDocumentManager, svcManager: ServiceManager, logout: boolean): Promise<any> {
-  infoDialog()
+function saveAndQuit(
+  app: JupyterFrontEnd,
+  docManager: IDocumentManager,
+  svcManager: ServiceManager,
+  logout: boolean
+): Promise<any> {
+  infoDialog();
   const retval = Promise.resolve(saveAll(app, docManager, svcManager));
-  retval.then((res) => {
-    return justQuit(app, docManager, svcManager, logout)
+  retval.then(res => {
+    return justQuit(app, docManager, svcManager, logout);
   });
-  retval.catch((err) => {
-    console.log("saveAll failed: ", err.message);
+  retval.catch(err => {
+    console.log('saveAll failed: ', err.message);
   });
-  console.log("Save and Quit complete.")
-  return retval
+  console.log('Save and Quit complete.');
+  return retval;
 }
 
-function justQuit(app: JupyterFrontEnd, docManager: IDocumentManager, svcManager: ServiceManager, logout: boolean): Promise<any> {
-  infoDialog()
-  let targetEndpoint = "/"
+function justQuit(
+  app: JupyterFrontEnd,
+  docManager: IDocumentManager,
+  svcManager: ServiceManager,
+  logout: boolean
+): Promise<any> {
+  infoDialog();
+  let targetEndpoint = '/';
   if (logout) {
-    targetEndpoint = "/logout"
+    targetEndpoint = '/logout';
   }
-  return Promise.resolve(hubDeleteRequest(app)
-    .then(() => {
-      console.log("Quit complete.")
-    })
-    .then(() => {
-      window.location.replace(targetEndpoint)
-    }))
+  return Promise.resolve(
+    hubDeleteRequest(app)
+      .then(() => {
+        console.log('Quit complete.');
+      })
+      .then(() => {
+        window.location.replace(targetEndpoint);
+      })
+  );
 }
 
 function infoDialog(): Promise<void> {
-  let options = {
-    title: "Redirecting to landing page",
-    body: "JupyterLab cleaning up and redirecting to landing page.",
+  const options = {
+    title: 'Redirecting to landing page',
+    body: 'JupyterLab cleaning up and redirecting to landing page.',
     buttons: [Dialog.okButton({ label: 'Got it!' })]
   };
   return showDialog(options).then(() => {
-    console.log("Info dialog panel displayed")
-  })
+    console.log('Info dialog panel displayed');
+  });
 }
 
 /**
@@ -174,12 +176,8 @@ function infoDialog(): Promise<void> {
 const rspSavequitExtension: JupyterFrontEndPlugin<void> = {
   activate: activateRSPSavequitExtension,
   id: token.SAVEQUIT_ID,
-  requires: [
-    IMainMenu,
-    IDocumentManager
-  ],
-  autoStart: false,
+  requires: [IMainMenu, IDocumentManager],
+  autoStart: false
 };
 
 export default rspSavequitExtension;
-
--- src/tokens.ts
+++ src/tokens.ts
@@ -3,10 +3,10 @@
  */
 /*import { Token } from '@lumino/coreutils';*/
 
-export const NS = 'rsp-jupyterlab'
+export const NS = 'rsp-jupyterlab';
 
-export const PLUGIN_ID = `${NS}:plugin`
+export const PLUGIN_ID = `${NS}:plugin`;
 
-export const DISPLAYVERSION_ID = `${NS}:displayversion`
-export const SAVEQUIT_ID = `${NS}:savequit`
-export const QUERY_ID = `${NS}:query`
+export const DISPLAYVERSION_ID = `${NS}:displayversion`;
+export const SAVEQUIT_ID = `${NS}:savequit`;
+export const QUERY_ID = `${NS}:query`;
