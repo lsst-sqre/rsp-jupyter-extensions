2037adfb48dde3e66ba8fb493cb248cbe8a63736
adam <athornton@lsst.org>
2023-10-26 15:23:30 -0700

    Fix ui test, remove unneeded file

--- ui-tests/tests/rsp_jupyter_extensions.spec.ts
+++ ui-tests/tests/rsp_jupyter_extensions.spec.ts
@@ -17,7 +17,7 @@ test('should emit an activation console message', async ({ page }) => {
 
   expect(
     logs.filter(
-      s => s === 'JupyterLab extension rsp-jupyter-extensions is activated!'
+      s => s === '...loaded rsp-lab-extension.'
     )
   ).toHaveLength(1);
 });
