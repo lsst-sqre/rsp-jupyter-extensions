8565350d2cbcba93fedccbcf1e5c0826cabb6853
adam <athornton@lsst.org>
2024-09-04 15:15:32 -0700

    Add GET-based portal query handler

--- rsp_jupyter_extensions/handlers/query.py
+++ rsp_jupyter_extensions/handlers/query.py
@@ -10,6 +10,7 @@ from ._rspclient import RSPClient
 import tornado
 
 from jupyter_server.base.handlers import JupyterHandler
+from urllib.parse import urljoin
 
 
 class UnsupportedQueryTypeError(Exception):
@@ -20,7 +21,7 @@ class UnimplementedQueryResolutionError(Exception):
     pass
 
 
-class Query_handler(JupyterHandler):
+class QueryHandler(JupyterHandler):
     """
     RSP templated Query Handler.
     """
@@ -32,33 +33,6 @@ class Query_handler(JupyterHandler):
     @property
     def rubinquery(self) -> dict[str, str]:
         return self.settings["rubinquery"]
-
-    @tornado.web.authenticated
-    def post(self) -> None:
-        """POST receives the query type and the query value as a JSON
-        object containing "type" and "value" keys.  Each is a string.
-
-        "type" is currently limited to "portal".
-
-        For a Portal Query, "value" is the URL referring to that query.
-        The interpretation of "value" is query-type dependent.
-
-        We should have some sort of template service.  For right now, we're
-        just going to go with a very dumb string substitution.
-
-        It will then use the value to resolve the template, and will write
-        a file with the template resolved under the user's
-        "$HOME/notebooks/queries" directory.  That filename will also be
-        derived from the type and value.
-        """
-        input_str = self.request.body.decode("utf-8")
-        input_document = json.loads(input_str)
-        q_type = input_document["type"]
-        q_value = input_document["value"]
-        if q_type != "portal":
-            raise UnsupportedQueryTypeError(f"{q_type} is not a supported query type")
-        q_fn = self._create_portal_query(q_value)
-        self.write(q_fn)
 
     def _create_portal_query(self, q_value: str) -> str:
         # The value should be a URL
@@ -110,3 +84,52 @@ class Query_handler(JupyterHandler):
         # string representing the rendered notebook "in unicode", which I
         # think means, "a string represented in the default encoding".
         return self._client.get(rendered_url, params=params).text
+
+
+class PostQueryHandler(QueryHandler):
+    @tornado.web.authenticated
+    def post(self) -> None:
+        """POST receives the query type and the query value as a JSON
+        object containing "type" and "value" keys.  Each is a string.
+
+        "type" is currently limited to "portal".
+
+        For a Portal Query, "value" is the URL referring to that query.
+        The interpretation of "value" is query-type dependent.
+
+        It will then use the value to resolve the template, and will write
+        a file with the template resolved under the user's
+        "$HOME/notebooks/queries" directory.  That filename will also be
+        derived from the type and value.
+        """
+        input_str = self.request.body.decode("utf-8")
+        input_document = json.loads(input_str)
+        q_type = input_document["type"]
+        q_value = input_document["value"]
+        if q_type != "portal":
+            raise UnsupportedQueryTypeError(f"{q_type} is not a supported query type")
+        q_fn = self._create_portal_query(q_value)
+        self.write(q_fn)
+
+
+class GetQueryHandler(JupyterHandler):
+    """
+    Get query with /rubin/portal_query/<q_id>.
+
+    This is a convenience route to make writing a redirection route
+    easier (since we just need to fill in q_id, and don't have to issue
+    a POST).
+    """
+
+    @tornado.web.authenticated
+    def get(self):
+        components = [x for x in self.request.path.split("/") if x]
+        if (len(components) < 2) or (components[-2] != "portal_query"):
+            raise UnimplementedQueryResolutionError(
+                f"{self.request.path} is not a supported query url"
+            )
+        q_id = components[-1]
+        base_url = self.request.host
+        q_url = str(urljoin(base_url, "/api/tap/async", q_id))
+        q_fn = self._create_portal_query(q_url)
+        self.write(q_fn)
