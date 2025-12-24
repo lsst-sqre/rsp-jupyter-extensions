9258648283925578e9765a610c90ff686dcef3da
adam <athornton@lsst.org>
2025-12-24 11:28:34 -0700

    Add front-end for PDF export

--- src/tokens.ts
+++ src/tokens.ts
@@ -6,6 +6,7 @@ export const NS = 'rsp-jupyterlab';
 export const PLUGIN_ID = `${NS}:plugin`;
 
 export const DISPLAYVERSION_ID = `${NS}:displayversion`;
-export const SAVEQUIT_ID = `${NS}:savequit`;
+export const PDFEXPORT_ID = `${NS}:pdfexport`;
 export const QUERY_ID = `${NS}:query`;
+export const SAVEQUIT_ID = `${NS}:savequit`;
 export const TUTORIALS_ID = `${NS}:tutorials`;
